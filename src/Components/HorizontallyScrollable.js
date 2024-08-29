bb79b725236339d378ab9247454ad6bca632e2e1
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-08-28 21:04:56 -0400

    mobile-friendly fixes with scrollbar

--- src/Components/HorizontallyScrollable.js
+++ src/Components/HorizontallyScrollable.js
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-function HorizontallyScrollable({children, className=''}) {
+function HorizontallyScrollable({ children, className = '' }) {
     const scrollRef = useRef();
 
     const handleMouseDown = (evt) => {
@@ -23,11 +23,36 @@ function HorizontallyScrollable({children, className=''}) {
         window.addEventListener('mouseup', handleMouseUp);
     };
 
+    const handleTouchStart = (evt) => {
+        const oldX = evt.touches[0].pageX;
+        const scrollLeft = scrollRef.current.scrollLeft;
+
+        const handleTouchMove = (evt) => {
+            const newX = evt.touches[0].pageX;
+            const offSet = newX - oldX;
+
+            scrollRef.current.scrollLeft = scrollLeft - offSet;
+        };
+
+        const handleTouchEnd = () => {
+            window.removeEventListener('touchmove', handleTouchMove);
+            window.removeEventListener('touchend', handleTouchEnd);
+        };
+
+        window.addEventListener('touchmove', handleTouchMove);
+        window.addEventListener('touchend', handleTouchEnd);
+    };
+
     return (
-        <div className={className}  ref={scrollRef} onMouseDown={(evt) => handleMouseDown(evt)}>
+        <div
+            className={className}
+            ref={scrollRef}
+            onMouseDown={(evt) => handleMouseDown(evt)}
+            onTouchStart={(evt) => handleTouchStart(evt)}
+        >
             {children}
         </div>
     );
-};
+}
 
 export default HorizontallyScrollable;
